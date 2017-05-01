872b606dd2ae0a841ee7b46b098461173d29991e
William Towe <willbur1984@gmail.com>
2017-05-01 19:13:46 -0400

    Initial Commit

---
+++ KSOWebKit/KSOWebKit.h
@@ -0,0 +1,19 @@
+//
+//  KSOWebKit.h
+//  KSOWebKit
+//
+//  Created by William Towe on 5/1/17.
+//  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSOWebKit.
+FOUNDATION_EXPORT double KSOWebKitVersionNumber;
+
+//! Project version string for KSOWebKit.
+FOUNDATION_EXPORT const unsigned char KSOWebKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSOWebKit/PublicHeader.h>
+
+
